aaf193b2df3e6bc55caa45b4c7c2bff145d72a3e
Slavik2706 <grinko-s@mail.ru>
2022-06-24 18:56:19 +0300

    Выполнил ДЗ №21

---
+++ src/modules/validForm.js
@@ -0,0 +1,37 @@
+
+const validForm = () => {
+   const calcInputs = document.querySelectorAll('.calc-block input')
+   const textInputs = document.querySelectorAll('form [type=text]')
+   const messageInput = document.querySelector('form [placeholder="Ваше сообщение"]')
+   const emailInputs = document.querySelectorAll('form [type=email]')
+   const phoneInputs = document.querySelectorAll('form [type=tel]')
+
+   calcInputs.forEach((calcInput) => {
+      calcInput.addEventListener('input', (event) => {
+         event.target.value = event.target.value.replace(/\D+/, "")
+      })
+   })
+   textInputs.forEach((textInput) => {
+      textInput.addEventListener('input', (event) => {
+         event.target.value = event.target.value.replace(/[^а-яА-Я- ]/, "")
+         console.log(event.target.value)
+      })
+
+   })
+   emailInputs.forEach((emailInput) => {
+      emailInput.addEventListener('input', (event) => {
+         event.target.value = event.target.value.replace(/[^a-zA-Z0-9@-_.!~*']/, "")
+      })
+   })
+   messageInput.addEventListener('input', (event) => {
+      event.target.value = event.target.value.replace(/[^а-яА-Я- ]/, "")
+   })
+   phoneInputs.forEach((phoneInput) => {
+      phoneInput.addEventListener('input', (event) => {
+         event.target.value = event.target.value.replace(/[^\d()-]/, "")
+      })
+   })
+}
+
+export default validForm
+
