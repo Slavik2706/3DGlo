d8998df6a6a548c15ded16aaa1e0deb6b18f5e4f
Slavik2706 <grinko-s@mail.ru>
2022-06-30 18:41:48 +0300

    Выполнил ДЗ №24

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from './modules/modal'
 import validForm from './modules/validForm'
 import tabs from './modules/tabs'
 import slider from './modules/slider'
+import calc from './modules/calc'
 
 timer('25 june 2022')
 menu()
@@ -11,3 +12,4 @@ modal()
 validForm()
 tabs()
 slider()
+calc(100)
